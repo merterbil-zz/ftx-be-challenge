cee372764e99e40311f13417f204ec20f8288b88
Mert Erbil <mert.erbil@leanscale.com>
2021-02-28 00:28:18 +0300

    quote refactoring

--- src/services/ftx.js
+++ src/services/ftx.js
@@ -4,7 +4,7 @@ import { Service, Container } from 'typedi';
 export default class FtxService {
   _ftx = Container.get('ftx');
 
-  _getMarketNames = async (baseCurrency, quoteCurrency) => {
+  _getMarket = async (baseCurrency, quoteCurrency) => {
     const market = [baseCurrency, quoteCurrency];
     const markets = await this._ftx.request({
       method: 'GET',
@@ -13,60 +13,50 @@ export default class FtxService {
     return (
       markets?.result
         ?.filter((item) => item.type === 'spot' && (item.name === market.join('/') || item.name === market.reverse().join('/')))
-        .map((item) => item.name) || []
+        .map((item) => item.name).pop() || []
     );
   };
 
-  _getOrderbook = async (market) => {
+  _getOrderbook = async (market, action) => {
     const orderbook = await this._ftx.request({
       method: 'GET',
       path: `/markets/${market}/orderbook`,
     });
-    return orderbook?.result || {};
+    return (action === 'buy' ? orderbook?.result?.bids : orderbook?.result?.asks) || [];
   };
 
   quote = async ({ action, base_currency, quote_currency, amount }) => {
-    const parsedAmount = parseFloat(amount);
+    let parsedAmount = parseFloat(amount);
     if (isNaN(parsedAmount)) {
-      throw new Error('amount cannot parsed')
+      throw new Error('amount cannot parsed');
     }
 
-    const markets = await this._getMarketNames(base_currency, quote_currency);
-    if (!markets.length) {
-      throw new Error('No market for requested currencies');
+    const market = await this._getMarket(base_currency, quote_currency);
+    if (!market) {
+      throw new Error('No market for requested currency pair');
     }
 
-    const orderbooks = await Promise.all(
-      markets.map(async (item) => {
-        const orderbook = await this._getOrderbook(item);
-        return {
-          market: item,
-          book: (action === 'buy' ? orderbook.bids : orderbook.asks) || [],
-        };
-      })
-    );
+    const orderbook = await this._getOrderbook(market, action);
+    if (!orderbook?.length) {
+      throw new Error('No orderbook for requested currency pair');
+    }
 
-    let total = 0;
-    orderbooks.forEach((item) => {
-      let _amount = parsedAmount;
-      item.book.forEach(([unitPrice, size]) => {
-        if (_amount === 0) {
-          return;
-        }
+    const total = orderbook.reduce((current, [unitPrice, size]) => {
+      if (parsedAmount === 0) return current;
 
-        if (_amount > size) {
-          total += size * unitPrice;
-          _amount -= size;
-        } else {
-          total += _amount * unitPrice;
-          _amount = 0;
-        }
-      });
-    });
+      if (parsedAmount > size) {
+        parsedAmount -= size;
+        return current + size * unitPrice;
+      }
+
+      const paymentPrice = parsedAmount * unitPrice;
+      parsedAmount = 0;
+      return current + paymentPrice;
+    }, 0) || 0;
 
     return {
       total: total.toFixed(8),
-      price: (total / amount).toFixed(8),
+      price: (total / parseFloat(amount)).toFixed(8),
       currency: quote_currency,
     };
   };
